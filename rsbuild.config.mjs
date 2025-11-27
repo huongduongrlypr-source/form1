47c46fae409e35d81cd1b4b06ca9f6a79dacd028
huongduongrlypr-source <huongduongrlypr@gmail.com>
2025-11-27 16:04:16 +0700

    Update rsbuild.config.mjs

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,30 +1,9 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import tailwindcss from '@tailwindcss/postcss';
-import fs from 'fs/promises';
-import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        pluginReact(),
-        {
-            name: 'plugin-htaccess-spa',
-            setup(api) {
-                api.onAfterBuild(async () => {
-                    const distPath = path.resolve('dist');
-                    const htaccessPath = path.join(distPath, '.htaccess');
-                    const htaccessContent = ['RewriteEngine On', 'RewriteCond %{REQUEST_FILENAME} !-f', 'RewriteCond %{REQUEST_FILENAME} !-d', 'RewriteRule ^ index.html [L]'].join('\n');
-                    try {
-                        await fs.access(distPath);
-                        await fs.writeFile(htaccessPath, htaccessContent);
-                        api.logger.info('htaccess build xong');
-                    } catch {
-                        api.logger.error('htaccess build fail');
-                    }
-                });
-            }
-        }
-    ],
+    plugins: [pluginReact()],
     tools: {
         postcss: {
             postcssOptions: {
@@ -32,25 +11,7 @@ export default defineConfig({
             }
         }
     },
-    resolve: {
-        alias: {
-            '@': './src'
-        }
-    },
     html: {
         title: 'Meta Security',
-        // ĐÃ XÓA FAVICON - KHÔNG CÒN LỖI
-    },
-    source: {
-        tsconfigPath: './jsconfig.json'
-    },
-    output: {
-        dataUriLimit: {
-            image: Number.MAX_SAFE_INTEGER,
-            svg: Number.MAX_SAFE_INTEGER,
-            font: Number.MAX_SAFE_INTEGER,
-            media: Number.MAX_SAFE_INTEGER,
-            assets: Number.MAX_SAFE_INTEGER
-        }
     }
 });
